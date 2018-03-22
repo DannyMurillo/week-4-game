edbde88e7113175a420414c027ecf8e73316c962
Daniel Murillo <danielmurillo@Daniels-MacBook-Pro.local>
2018-03-22 16:11:48 -0700

    more work done

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// this will house the entirity of the code for the game
+var CrystalCollectorGame = {
+
+// variables that will start with initial values or 0
+    total: 0,               //total score add up of the hidden value gems
+    CrystalValues: [],      //values of all of the gems 
+    CrystalChosen: [],      //which crystals are chosen which would then be added in the total
+    wins: 0,                //number of times that the user matches the total with the randomN
+    losses: 0,              //number of losses if the total goes over the random number
+    randomN: 0,             //value you are trying to hit
+
+    //setup that initializes the game when the screen is loaded up
+    setUpGame: function () {
+        
+    };
+}
